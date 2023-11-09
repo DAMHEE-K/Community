8edf1bef87064f21cab76396c56554fe546c9582
DAMHEE-K <126038575+DAMHEE-K@users.noreply.github.com>
2023-11-09 16:39:43 +0900

    home 추가
    .

--- front/client/src/routes/Home.js
+++ front/client/src/routes/Home.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import profileImg from '../images/profile.png'
-import './Home.css'
+import React, { useState, useEffect } from 'react';
+import profileImg from '../images/profile.png';
 import ProjectList from '../component/ProjectList';
+import './Home.css';
 
 const Home = () => {
+  const [letters, setLetters] = useState('');
+  const [count, setCount] = useState(0);
+  const text = "DAMHEE's Community";
+
+  useEffect(() => {
+      const typingInterval = setInterval(() => {
+        setLetters((pretext) => {
+          let result = pretext ? pretext + text[count] : text[0];
+          setCount(count + 1);
+
+          if(count >= text.length) {
+            setCount(0);
+            setLetters('');
+          }
+
+          return result;
+        });
+      }, 300);
+
+      return() => {
+        clearInterval(typingInterval);
+      };
+  });
+
   return (
-  <div className='container'>
-    홈화면
-  </div>
+    <div className='container'>
+      <span className='intro-letter'>{letters}</span>
+    </div>
   );
 };
 
